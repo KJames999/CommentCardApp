b906fe2adcae16c74042131ddf1467a1acac1b9e
Killian James <james.k@etoncollege.org.uk>
2022-02-08 11:18:58 +0000

    MVP comment card writer

--- CommentCardApp/ContentView.swift
+++ CommentCardApp/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var options = Options()
+    @State private var selectedOption = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            VStack {
+                Text("I am finding this subject \(options.feelings[selectedOption])")
+                    .padding()
+                Button("Next option", action: {
+                    if selectedOption == 4 {
+                        selectedOption = 0
+                    } else {
+                        selectedOption += 1
+                    }
+                })
+            }
+        }
     }
 }
 
---
+++ CommentCardApp/Options.swift
@@ -0,0 +1,13 @@
+//
+//  Options.swift
+//  CommentCardApp
+//
+//  Created by James, Killian (NA) on 08/02/2022.
+//
+
+import Foundation
+
+class Options: ObservableObject {
+    let feelings = ["Very Hard", "Hard", "Okay", "Easy", "Very Easy"]
+    
+}
