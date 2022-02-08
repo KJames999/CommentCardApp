0d92145ddb29ecde99b21ff997022caec5d89571
Killian James <james.k@etoncollege.org.uk>
2022-02-08 10:52:45 +0000

    Initial Commit

---
+++ CommentCardApp/CommentCardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardAppApp.swift
+//  CommentCardApp
+//
+//  Created by James, Killian (NA) on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
